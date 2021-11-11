20b326cbc2266f9dd9bc53cf58e0f54a8b554a37
Eysen-ZZT <2465597618@qq.com>
2021-11-11 12:25:50 +0800

    基础类型object，Array，enum，Tuple

--- code/chapter01/03_types.js
+++ code/chapter01/03_types.js
@@ -23,8 +23,47 @@ if (typeof value === 'string') {
  */
 s = value;
 s = value;
+// void 类型
 function warnUser() {
     console.log("This is my warning message");
     // return 123; // Type 'number' is not assignable to type 'void'
     return null; // ok 
 }
+// enum 类型
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var colorName1 = Color[1]; // 'Red'
+var colorName2 = Color[2]; // 'Green'  因为上面代码里它的值是2
+var c1 = Color.Red; // 1
+var c12 = Color[Color[1]]; // 1
+var c2 = Color.Green; // 2
+// let c22: string = Color.Green; // Type 'Color' is not assignable to type 'string'
+// console.log(typeof c12); // number
+// 数字枚举
+var Direction;
+(function (Direction) {
+    Direction[Direction["NORTH"] = 3] = "NORTH";
+    Direction[Direction["SOUTH"] = 4] = "SOUTH";
+    Direction[Direction["EAST"] = 5] = "EAST";
+    Direction[Direction["WEST"] = 6] = "WEST";
+})(Direction || (Direction = {}));
+var dir = Direction[3]; // NORTH
+var dir2 = Direction.NORTH; // 3
+// 字符串枚举
+var Direction2;
+(function (Direction2) {
+    Direction2["NORTH2"] = "NORTH";
+    Direction2["SOUTH2"] = "SOUTH";
+    Direction2["EAST2"] = "EAST";
+    Direction2["WEST2"] = "WEST";
+})(Direction2 || (Direction2 = {}));
+var dir3 = Direction2[0]; // undefined
+var dir4 = Direction2[0]; // undefined
+var dir5 = Direction2[Direction2.NORTH2]; // undefined
+// 以上两个不管怎么替换都是undefined
+var dir6 = Direction2.NORTH2; // NORTH
+// 通过上面的代码对比，可以发现数字枚举相对字符串枚举多了 “反向映射”
--- code/chapter01/03_types.ts
+++ code/chapter01/03_types.ts
@@ -27,8 +27,44 @@ if (typeof value === 'string') {
 s = value as string
 s = <string>value
 
+// void 类型
 function warnUser(): void {
     console.log("This is my warning message");
     // return 123; // Type 'number' is not assignable to type 'void'
     return null; // ok 
 }
+
+// enum 类型
+enum Color { Red = 1, Green, Blue }
+let colorName1: string = Color[1]; // 'Red'
+let colorName2: string = Color[2]; // 'Green'  因为上面代码里它的值是2
+let c1: Color = Color.Red; // 1
+let c12: string = Color[Color[1]]; // 1
+let c2: Color = Color.Green; // 2
+// let c22: string = Color.Green; // Type 'Color' is not assignable to type 'string'
+// console.log(typeof c12); // number
+
+// 数字枚举
+enum Direction {
+    NORTH = 3,
+    SOUTH = 4,
+    EAST,
+    WEST,
+}
+let dir: string = Direction[3]; // NORTH
+let dir2: Direction = Direction.NORTH; // 3
+
+// 字符串枚举
+enum Direction2 {
+    NORTH2 = "NORTH",
+    SOUTH2 = "SOUTH",
+    EAST2 = "EAST",
+    WEST2 = "WEST",
+}
+let dir3: Direction2 = Direction2[0]; // undefined
+let dir4: string = Direction2[0]; // undefined
+let dir5: Direction2 = Direction2[Direction2.NORTH2]; // undefined
+// 以上两个不管怎么替换都是undefined
+let dir6: Direction2 = Direction2.NORTH2; // NORTH
+
+// 通过上面的代码对比，可以发现数字枚举相对字符串枚举多了 “反向映射”
